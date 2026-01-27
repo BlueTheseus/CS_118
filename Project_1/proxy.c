5947824746590da0e77ed3746d8b64b5db29dfbc
Evan Aceves <evanm.aceves@gmail.com>
2026-01-26 23:30:15 -0800

    add Media Type for txt file extension and corrected Media Type for binary files

--- Project_1/proxy.c
+++ Project_1/proxy.c
@@ -255,15 +255,18 @@ void send_local_file(SSL *ssl, const char *path) {
     if (strstr(path, ".html")) {
         response = "HTTP/1.1 200 OK\r\n"
                    "Content-Type: text/html; charset=UTF-8\r\n\r\n";
+	} else if (strstr(path, ".txt")) {
+        response = "HTTP/1.1 200 OK\r\n"
+                   "Content-Type: text/plain; charset=UTF-8\r\n\r\n";
 	} else if (strstr(path, ".jpg")) {
         response = "HTTP/1.1 200 OK\r\n"
                    "Content-Type: image/jpeg; charset=UTF-8\r\n\r\n";
 	} else if (strstr(path, ".m3u8")) {
         response = "HTTP/1.1 200 OK\r\n"
                    "Content-Type: application/vnd.apple.mpegurl; charset=UTF-8\r\n\r\n";
-    } else { // including .txt
+    } else {
         response = "HTTP/1.1 200 OK\r\n"
-                   "Content-Type: text/plain; charset=UTF-8\r\n\r\n";
+                   "Content-Type: application/octet-stream; charset=UTF-8\r\n\r\n";
     }
 
     // Send response header and file content via SSL
